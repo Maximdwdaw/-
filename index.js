f68901f7747555a7b91209e4e17cb879d7193c36
Maximdwdaw <142977598+Maximdwdaw@users.noreply.github.com>
2023-11-15 00:35:56 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 // index.js
+const express = require('express');
+const app = express();
 
-module.exports = (req, res) => {
-  // Производим перенаправление на нужную страницу
-  res.writeHead(302, { Location: 'https://eat3-6c2e9b61596b.herokuapp.com/' });
-  res.end();
-};
+const PORT = process.env.PORT || 3000;
+
+app.use((req, res) => {
+  // Производим перенаправление на нужный сайт
+  res.redirect('https://eat3-6c2e9b61596b.herokuapp.com/');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
